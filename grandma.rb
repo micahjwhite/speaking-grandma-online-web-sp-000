0387ef779b106a9ceffa928d9a6e4a0a5a68137c
Micah White <micahjwhite@gmail.com>
2020-04-09 18:52:01 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,9 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
+def speak_to_grandma(phrase)
+  if speak_to_grandma(phrase)
+    puts "HUH?! SPEAK UP, SONNY!"
+  end
+end
